871a9631854763d021d192a9956cd4522b523ff4
rickeydidio <rickeydidio@gmail.com>
2021-09-15 21:27:02 -0500

    Update hackerrank.sql

--- hackerrank.sql
+++ hackerrank.sql
@@ -3,3 +3,8 @@ FROM city ci, customer cu, invoice i, invoice_item ii, product pr
 WHERE ci.id = cu.city_id AND cu.id = i.customer.id AND i.id = ii.invoice.id AND ii.product_id = pr.id
 GROUP BY ci.city_name, pr.product_name
 ORDER BY tot DESC, ci.city_name, pr.product_name;
+
+
+SELECT (months*salary) as earnings, COUNT(*) FROM employee
+GROUP BY earnings
+ORDER BY earnings DESC LIMIT 1;
